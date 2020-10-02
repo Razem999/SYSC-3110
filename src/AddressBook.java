59d3463693c1e83b458a715bb4b2751bb8ebc2df
Razem <razemshahin@cmail.carleton.ca>
2020-10-01 23:59:56 -0400

    Lab 3 Testing

---
+++ src/AddressBook.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class AddressBook {
+
+    private List<BuddyInfo> budInf;
+    public AddressBook() {
+        budInf = new ArrayList<>();
+    }
+
+    private void addBuddy(BuddyInfo BI ) {
+        if(BI != null) {
+            budInf.add(BI);
+        }
+    }
+
+    public BuddyInfo removeBuddy(int index) {
+        if(index >= 0 && index < budInf.size()) {
+            return budInf.remove(index);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(0);
+        System.out.println("Address Book");
+    }
+}
